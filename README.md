f82291a3727471911ed071e16ba9f41f173e23ac
niosanz20 <80337570+niosanz20@users.noreply.github.com>
2021-03-27 00:00:01 +0800

    updated

--- README.md
+++
@@ -1,2 +0,0 @@
-# rclc
-  Construction Project Employee Attendance and Project Materials Cost Monitoring System
--- admin/includes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="main-footer">
-  <div class="pull-right hidden-xs">
-  </div>
-  <strong>Copyright 2021 &copy; Construction Project Employee Attendance and Project Materials Cost Monitoring System </strong>
-</footer>

45653f7d2c92e171805d2f264e7ed0469ddcbe21
niosanz20 <80337570+niosanz20@users.noreply.github.com>
2021-06-13 18:24:27 +0800

    Some Changes in Payslip

--- admin/php/overtime/load_overtimeRecordReports.php
+++ admin/php/overtime/load_overtimeRecordReports.php
@@ -2,7 +2,7 @@
 include '../../includes/conn.php';
 
 if (isset($_POST['status'])) {
-	$output = '<table id="example1" class="table table-bordered">
+    $output = '<table id="example1" class="table table-bordered">
                 <thead>
                     <th>Name</th>
                     <th>Project Name</th>
@@ -17,7 +17,7 @@ if (isset($_POST['status'])) {
 
     $status = $_POST['status'];
     $project_id = $_POST['project_id'];
-	$cutoffID = $_POST['cutoff_id'];
+    $cutoffID = $_POST['cutoff_id'];
 
     $sqlCutoff =  !empty($cutoffID) ? "AND payslip.cutoff_id='$cutoffID'" : "AND payslip.cutoff_id=(SELECT MAX(payslip.cutoff_id) FROM cutoff)";
     $sqlProject = !empty($project_id) ? "AND project_employee.projectid = '$project_id'" : "";
@@ -38,37 +38,37 @@ if (isset($_POST['status'])) {
 
     $result = $conn->query($sql);
     $projectName = $cutoffDate = "";
-	while ($row = $result->fetch_assoc()) {
+    while ($row = $result->fetch_assoc()) {
         $ot_status = "";
         if ($row['ot_status'] == "Approved") {
             $ot_status = "
-            <span class='label label-success' style='padding: 10px'><i class=' glyphicon glyphicon-ok-circle'></i> Approved on ".  $row['timestamp']  ."</span>
+            <span class='label label-success' style='padding: 10px'><i class=' glyphicon glyphicon-ok-circle'></i> Approved on " .  $row['timestamp']  . "</span>
                 ";
         } else if ($row['ot_status'] == "Declined") {
             $ot_status = "
-            <span class='label label-danger' style='padding: 10px'><i class=' glyphicon glyphicon-ban-circle'></i> Declined on ".  $row['timestamp']  ."</span>
+            <span class='label label-danger' style='padding: 10px'><i class=' glyphicon glyphicon-ban-circle'></i> Declined on " .  $row['timestamp']  . "</span>
                     ";
-            }
-		$output .= "
+        }
+        $output .= "
             <tr>
                 <td><strong>" . $row['firstname'] . ' ' . $row['lastname'] . '</strong> | ' . $row['description'] . "</td>
-                <td>".$row['project_name']."</td>
+                <td>" . $row['project_name'] . "</td>
                 <td>" . date('h:i A', strtotime($row['stime_in'])) . ' - ' . date('h:i A', strtotime($row['stime_out'])) . "</td> 
                 <td><strong>" . date('M d, Y', strtotime($row['date_overtime'])) . '</strong> | ' . date('h:i A', strtotime($row['ttime_in'])) . ' - ' . date('h:i A', strtotime($row['ttime_out'])) . "</td>
                 <td>" . $row['hours'] . "</td>
                 <td>" . number_format($row['amount'], 2) . "</td>
                 <td>  $ot_status </td>
-                <td>".  $row['reason']  ."</td>
+                <td>" .  $row['reason']  . "</td>
             </tr>";
-	}
+    }
 
-	$output .= '</tbody>
+    $output .= '</tbody>
             </table>
             <div class="row">
                 <!-- button here -->
             </div>
             ';
-	$output .= "
+    $output .= "
 		 <script>
 			$('#example1').DataTable({
 		  responsive: true,
@@ -81,12 +81,12 @@ if (isset($_POST['status'])) {
 		})
 		 </script>
 		";
-   
-  
-     
-	$data = array(
-		'output' => $output
-	);
 
-	echo json_encode($data);
+
+
+    $data = array(
+        'output' => $output
+    );
+
+    echo json_encode($data);
 }

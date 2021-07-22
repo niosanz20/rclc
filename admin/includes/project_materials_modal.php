ce60b66e644a76045cdde6259b236efa1da6ead4
christopherdiuyan <christopher.diuyan@gmail.com>
2021-07-23 01:00:51 +0800

    Updated functions

--- admin/includes/project_materials_modal.php
+++ admin/includes/project_materials_modal.php
@@ -93,38 +93,46 @@ $projectname = $_GET['projectname'];
             <div class="modal-body">
                 <form class="form-horizontal" method="POST" action="project_material_edit.php"
                     enctype="multipart/form-data">
-                    <input type="hidden" id="projmaterid" name="id">
+                    <input type="hidden" id="edit_projmaterid" name="id">
                     <div class="form-group">
-                        <label for="description" class="col-sm-3 control-label">Description</label>
+                        <label for="description" class="col-sm-3 control-label material">Description</label>
                         <div class="col-sm-9">
-                            <input class="form-control" id="edit_description" name="description" required></input>
+                            <input class="form-control" type="text" id="edit_description" name="description" disabled></input>
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="quantity" class="col-sm-3 control-label">Quantity</label>
                         <div class="col-sm-9">
-                            <input class="form-control" id="edit_quantity" name="quantity" pattern="[0-9]+"
-                                required></input>
+                        <input class="form-control" type="number" id="edit_quantity" min="1" name="quantity" pattern="[0-9]+" value="1" onchange="Edit_CalculateAmountCost(this.value)" required
+                                ></input>
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="unit" class="col-sm-3 control-label">Unit</label>
                         <div class="col-sm-9">
-                            <input class="form-control" id="edit_unit" name="unit" required></input>
+                            <input class="form-control" id="edit_unit" name="unit" disabled></input>
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="unit_cost" class="col-sm-3 control-label">Unit Cost</label>
                         <div class="col-sm-9">
                             <input class="form-control" id="edit_unit_cost" name="unit_cost" pattern="[0-9.]+"
-                                required></input>
+                            disabled></input>
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="amnt_cost" class="col-sm-3 control-label">Amount Cost</label>
                         <div class="col-sm-9">
-                            <input class="form-control" id="edit_amnt_cost" name="amnt_cost" pattern="[0-9.]+"
-                                required></input>
+                            <input class="form-control" id="edit_total_amount" name="amnt_cost" pattern="[0-9.]+"
+                            disabled></input>
+                        </div>
+                    </div>
+                     <div class="form-group">
+                        <div class="col-sm-9">
+                            <input type="hidden" value="<?php echo $project_id ?>" ; class="form-control"
+                                name="projectid" id="projectid"></input>
+                            <input type="hidden" value="" name="totalAmount" id="edit_totalAmount">
+                            <input type="hidden" value="" name="materialId" id="edit_materialId">
                         </div>
                     </div>
             </div>
@@ -169,10 +177,7 @@ $projectname = $_GET['projectname'];
     </div>
 </div>
 
-
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script type="text/javascript" src="script/getData.js"></script>
-<script src=".../js/jquery-3.2.1.min.js"></script>
 
 <script>
     function GetMaterialPrice(){
@@ -199,7 +204,8 @@ $projectname = $_GET['projectname'];
     }
 
     function CalculateAmountCost(quantity){
-        let value = GetMaterialPrice() !== 0 ? quantity * GetMaterialPrice() : "Material Item is required!";
+        let price = GetMaterialPrice();
+        let value = price !== 0 ? quantity * price : "Material Item is required!";
         document.getElementById('total_amount').value = value;
         document.getElementById('totalAmount').value = value;
     }
@@ -210,5 +216,20 @@ $projectname = $_GET['projectname'];
     $(document).on('change', '.description', function() {
         let quantity = $('#quantity').val();
         CalculateAmountCost(quantity);
+    });
+
+    function Edit_CalculateAmountCost(quantity){
+        let price = $('#edit_unit_cost').val();
+        let value = price !== 0 ? quantity * price : "Material Item is required!";
+        document.getElementById('edit_total_amount').value = value;
+        document.getElementById('edit_totalAmount').value = value;
+    }
+
+    /*--
+    Auto-calculation for adding new materials on project
+    -----------------------------------*/
+    $(document).on('change', '.description', function() {
+        let quantity = $('#edit_quantity').val();
+        Edit_CalculateAmountCost(quantity);
     });
 </script>
